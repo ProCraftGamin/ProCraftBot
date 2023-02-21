138a4ad428b395010f8084aef18c02955aa9bfa8
ProCraftGamin <102633420+ProCraftGamin@users.noreply.github.com>
2023-02-21 12:27:59 -0800

    Update test2.js

--- commands/other/test2.js
+++ commands/other/test2.js
@@ -5,31 +5,6 @@ module.exports = {
 		.setName('test')
 		.setDescription('-'),
 	async execute(interaction) {
-		let returnMessage = '';
-
-		// generate top border
-		for (let i = 0; i < 13; i++) {
-			returnMessage = returnMessage + '🟩';
-		}
-		returnMessage = returnMessage + '\n';
-
-		// generate snake area
-		// height loop
-		for (let i = 0; i < 10; i++) {
-			returnMessage = returnMessage + '🟩';
-
-			// width loop
-			for (let j = 0; j < 11; j++) {
-				returnMessage = returnMessage + '⬛';
-			}
-			returnMessage = returnMessage + '🟩\n';
-		}
-		// generate bottom border
-		for (let i = 0; i < 13; i++) {
-			returnMessage = returnMessage + '🟩';
-		}
-
-		console.log(returnMessage);
-		await interaction.reply(returnMessage);
+		await interaction.reply(interaction.guild.scheduledEvents);
 	},
 };
