19b74055c6b0ed940c99e26890836d61adb56d94
ProCraftGamin <102633420+ProCraftGamin@users.noreply.github.com>
2023-03-09 11:08:58 -0800

    removed role checks from running at startup

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,6 @@ module.exports = {
 		client.user.setActivity('for /help || V2.05', { type: ActivityType.Watching });
 		generalFunctions.unscrambleGame(client);
 		generalFunctions.cmdFunctions(client);
-		generalFunctions.checkRoles(client);
 		twitchFunctions.liveCheck(client);
 		setInterval(generalFunctions.checkRoles, 43200000, client);
 		setInterval(twitchFunctions.liveCheck, 30000, client);
